ba578f89deb3b5566073bd1eab2b45ac5edcf452
marion <marcosmariondev@gmail.com>
2019-10-01 17:48:06 -0300

    Estilização do carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,105 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>Tênis</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
